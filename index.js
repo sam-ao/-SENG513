bcc56ff78d4f2edc000e24de94d968029636df31
sam-ao <sam_ao8@hotmail.com>
2017-03-10 16:58:38 -0700

    Done

--- index.js
+++ index.js
@@ -10,9 +10,182 @@ http.listen( port, function () {
 
 app.use(express.static(__dirname + '/public'));
 
+var users = new Map();
 // listen to 'chat' messages
 io.on('connection', function(socket){
+	socket.emit('getCookie');
+	users.set(socket, {nickname: "", color: "#111111"});
+	var greeting_message;
+	socket.on('foundCookie', function(cookie){
+		var nameExists = false;
+		users.forEach(function(user, socket){
+			if(user.nickname == cookie.nickname) {
+				nameExists = true;
+			}
+		});
+		if(!nameExists) {
+			users.set(socket, {nickname: cookie.nickname, color: cookie.color});
+		}
+		else {
+			users.set(socket, {nickname: generateNickname(), color: "#111111"});
+		}
+		socket.emit('setNickname', users.get(socket).nickname);
+		socket.emit('setColor', users.get(socket).color);
+		socket.emit('chatlog', chatLog);
+		socket.emit('chat',"Welcome to the chatroom, <font color=\"" + users.get(socket).color + "\">" 
+	    					+ users.get(socket).nickname + "<\/font>.");
+		greeting_message = timeStamp()
+	    					+ " <font color=\"" + users.get(socket).color + "\">" 
+	    					+ users.get(socket).nickname + "<\/font> joined the chatroom.";
+		socket.broadcast.emit('chat', greeting_message);
+		storeMessage(greeting_message);
+		updateAllUsers();
+	});
+	socket.on('notFoundCookie', function(){
+		users.set(socket, {nickname: generateNickname(), color: "#111111"});
+		socket.emit('setNickname', users.get(socket).nickname);
+		socket.emit('setColor', users.get(socket).color);
+		socket.emit('chatlog', chatLog);
+		socket.emit('chat',"Welcome to the chatroom, <font color=\"" + users.get(socket).color + "\">" 
+	    					+ users.get(socket).nickname + "<\/font>.");
+		greeting_message = timeStamp()
+	    					+ " <font color=\"" + users.get(socket).color + "\">" 
+	    					+ users.get(socket).nickname + "<\/font> joined the chatroom.";
+		socket.broadcast.emit('chat', greeting_message);
+		storeMessage(greeting_message);
+		updateAllUsers();
+	});
     socket.on('chat', function(msg){
-	io.emit('chat', msg);
-    });
+    	if(!checkCommands(socket, msg)){
+	    	var message = timeStamp()
+	    				+ " <font color=\"" + users.get(socket).color + "\">" + users.get(socket).nickname + "<\/font>: " 
+	    				+ msg;
+	    	var bolded_message = "<b>"+message+"<\/b>";
+	    	storeMessage(message);
+	    	socket.broadcast.emit('chat', message);
+	    	socket.emit('chat', bolded_message);
+		}
+	});
+
+    socket.on('disconnect', function(){
+    	var goodbye_message = timeStamp()
+	    			+ " <font color=\"" + users.get(socket).color + "\">" 
+	    			+ users.get(socket).nickname + "<\/font> left the chatroom.";
+    	socket.broadcast.emit('chat', goodbye_message);
+    	storeMessage(goodbye_message);
+    	users.delete(socket);
+    	updateAllUsers();
+  	});
 });
+function checkCommands(socket, msg) {
+	switch(msg.split(" ")[0]){
+		case '/nick':
+			var newNick = msg.split(" ")[1];
+			var nameExists = false;
+			users.forEach(function(user, socket){
+				if(user.nickname == newNick) {
+					nameExists = true;
+				}
+			});
+			if(!msg.split(" ")[1] || msg.split(" ")[2]){
+				socket.emit('chat',"Invalid nickname.");
+			}
+			else if(!nameExists) {
+				users.set(socket, {nickname: newNick, color: users.get(socket).color});
+				socket.emit('setNickname', users.get(socket).nickname);
+				updateAllUsers();
+				socket.emit('chat',"Nickname changed to " + newNick + ".");
+			}
+			else {
+				socket.emit('chat',"That nickname is already taken.");
+			}
+			return true;
+		break;
+
+		case '/nickcolor':
+			var newColor = msg.split(" ")[1];
+			var rgb = /[0-9A-Fa-f]{6}/i;
+			if(rgb.test(newColor)) {
+				users.set(socket, {nickname: users.get(socket).nickname, color: newColor});
+				updateAllUsers();
+				socket.emit('setColor', users.get(socket).color);
+				socket.emit('chat',"<font color=\""+users.get(socket).color+"\">Font color updated.<\/font>");
+			}
+			else {
+				socket.emit('chat',"Invalid color code.");
+			}
+			return true;
+		break;
+	}
+}
+var chatLog = [];
+
+function storeMessage(msg) {
+	if(chatLog.length < 200) {
+		chatLog.push(msg);
+	}
+	else {
+		chatLog.shift();
+		chatLog.push(msg);
+	}
+}
+
+var userList = [];
+var userCount = 0;
+
+function updateAllUsers() {
+	userList = [];
+	users.forEach(function(user, socket){
+		userList.push(user);
+	});
+    userList.sort(function(a, b){
+		return a.nickname < b.nickname ? -1 : 1;
+	});
+	io.emit('userlist', userList);
+}
+
+function generateNickname() {
+	userCount++;
+	var userNumber;
+	if(userCount<10) {
+		userNumber = '00'+userCount;
+	}
+	else if (userCount<100) {
+		userNumber = '0'+userCount;
+	}
+	else {
+		userNumber = userCount;
+	}
+	var nickname = 'User'+userNumber;
+	return nickname;
+}
+
+function timeStamp() {
+	var time = new Date();
+	var dd = time.getDate();
+	var mm = time.getMonth()+1; //January is 0!
+	var yyyy = time.getFullYear();
+	var hours = time.getHours();
+	var minutes = time.getMinutes();
+	var seconds = time.getSeconds();
+
+	if(dd<10) {
+	    dd='0'+dd;
+	} 
+	if(mm<10) {
+	    mm='0'+mm;
+	} 
+	if(hours<10) {
+	    hours='0'+hours;
+	} 
+	if(minutes<10) {
+	    minutes='0'+minutes;
+	}
+	if(seconds<10) {
+	    seconds='0'+seconds;
+	}
+
+	var timeStamp = mm+'/'+dd+'/'+yyyy+', '+hours+':'+minutes+':'+seconds;
+	var coloredTimeStamp = "<font color=\"#aaaaaa\">" + timeStamp + "<\/font>"
+	return coloredTimeStamp;
+}
--- public/client.js
+++ public/client.js
@@ -2,11 +2,78 @@
 $(function() {
     var socket = io();
     $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
-	return false;
+		socket.emit('chat', $('#m').val());
+		$('#m').val('');
+		return false;
     });
+
+    socket.on('connection');
+
+    socket.on('getCookie', function(){
+    	var nameVariable = "nickname=";
+    	var colorVariable = "color=";
+	    var decodedCookie = decodeURIComponent(document.cookie);
+	    var ca = decodedCookie.split(';');
+	    var nickname;
+	    var color;
+	    for(var i = 0; i < ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0) == ' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(nameVariable) == 0) {
+	        	nickname = c.substring(nameVariable.length, c.length);
+	        }
+	        if (c.indexOf(colorVariable) == 0) {
+	        	color = c.substring(colorVariable.length, c.length);
+	        }
+	    }
+	    if(nickname && color){
+	    	socket.emit('foundCookie', {nickname, color});
+	    }
+	    else {
+	    	socket.emit('notFoundCookie');
+	    }
+    });
+
+    socket.on('chatlog', function(messagelog){
+    	for(i = 0; i < messagelog.length; i++) {
+    		$('#messages').append($('<li>').html(messagelog[i]));
+    	}
+    	var messageBox = document.getElementById("messages");
+      	messageBox.scrollTop = messageBox.scrollHeight;
+    });
+
+    socket.on('setNickname', function(nickname){
+    	$('#greeting').text("Hi, "+nickname);
+    	var date = new Date();
+	    date.setTime(date.getTime() + (30*24*60*60*1000));
+	    var expires = "expires="+ date.toUTCString();
+	    document.cookie = "nickname=" + nickname + ";" + expires + ";path=/";
+    });
+
+    socket.on('setColor', function(color){
+		var date = new Date();
+	    date.setTime(date.getTime() + (30*24*60*60*1000));
+	    var expires = "expires="+ date.toUTCString();
+	    document.cookie = "color=" + color + ";" + expires + ";path=/";
+    });
+
+    socket.on('userlist', function(userlist){
+    	$('#user-list').empty();
+    	for(let user of userlist) {
+    		$('#user-list').append($('<li>').html("<font color=\""+user.color+"\">"+user.nickname+"<\/font>"));
+    	}
+    	/*for(i = 0; i < userlist.length; i++) {
+    		$('#user-list').append($('<li>').text(userlist[i].nickname));
+    	}*/
+    });
+
     socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+		$('#messages').append($('<li>').html(msg));
+		var messageBox = document.getElementById("messages");
+      	messageBox.scrollTop = messageBox.scrollHeight;
     });
+
+
 });
